8c370d8ca3d4437ae1aed7f50a13b4bfde259bd2
ishmamAli786 <ishmamali2200@gmail.com>
2023-01-19 19:59:52 +0500

    minor changes

--- src/Common Components/Navbar/Navbarbefor.jsx
+++ src/Common Components/Navbar/Navbarbefor.jsx
@@ -6,85 +6,105 @@ import { IoIosMenu, IoMdClose } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 const NavbarOne = () => {
-    const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar);
-    return (
-      <>
-        <nav className="topbar">
-          <div className="topbar-container">
-            <div className="topbar-container-logo">
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+  return (
+    <>
+      <nav className="topbar">
+        <div className="topbar-container">
+          <div className="topbar-container-logo">
+            <img src={logo} alt="logo" />
+            <h1 className="logo-text">Mental Health</h1>
+          </div>
+          <div className="topbar-container-menu">
+            <Link to="/" className="link-items">
+              Forum
+            </Link>
+            <Link to="/alldiscussion" className="link-items">
+              All Discussation
+            </Link>
+            <Link to="/help" className="link-items">
+              Form Guidline
+            </Link>
+          </div>
+          <div className="topbar-container-btn">
+            <Link to="/login" className="topbar-container-btn-first">
+              Log In
+            </Link>
+            <Link to="/signin" className="topbar-container-btn-second">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="responsive-topbar">
+        <div
+          className={`${
+            sidebar ? "side_menu-active" : "responsive-topbar-top "
+          }`}
+        >
+          {/* Responsive Bar  */}
+          <div className="responsive-topbar-container">
+            <div className="responsive-topbar-top-logo">
               <img src={logo} alt="logo" />
-              <h1 className="logo-text">Mental Health</h1>
+              <h1>Mental Health</h1>
             </div>
-            <div className="topbar-container-menu">
-              <Link to="/" className="link-items">Forum</Link>
-              <Link to="/alldiscussion" className="link-items">All Discussation</Link>
-              <Link to="/help" className="link-items">Form Guidline</Link>
-            </div>
-            <div className="topbar-container-btn">
-              <Link to="/login" className="topbar-container-btn-first">Log In</Link>
-              <Link to="/signin" className="topbar-container-btn-second">Sign Up</Link>
+
+            <div className="responsive-topbar-top-hamburger">
+              <Link>
+                {sidebar ? (
+                  <IoMdClose
+                    className="cross-icons"
+                    onClick={showSidebar}
+                    style={{ color: "" }}
+                  />
+                ) : (
+                  <IoIosMenu
+                    className="cross-icons"
+                    onClick={showSidebar}
+                    // style={{ color: "$skybl" }}
+                  />
+                )}
+              </Link>
             </div>
           </div>
-        </nav>
-  
-        <div className="responsive-topbar">
+
+          {/* Responsive Container */}
+
           <div
-            className={`${
-              sidebar ? "side_menu-active" : "responsive-topbar-top "
-            }`}
+            className={`${sidebar ? "contact-active" : "contact-active-none"}`}
           >
-            {/* Responsive Bar  */}
-            <div className="responsive-topbar-container">
-              
-              
-              
-              <div className="responsive-topbar-top-logo">
-                <img src={logo} alt="logo" />
-                <h1>Mental Health</h1>
-              </div>
-  
-              <div className="responsive-topbar-top-hamburger">
-                <Link>
-                  {sidebar ? (
-                    <IoMdClose className="cross-icons" onClick={showSidebar} style={{ color: "" }} />
-                  ) : (
-                    <IoIosMenu
-                      className="cross-icons"
-                      onClick={showSidebar}
-                      // style={{ color: "$skybl" }}
-                    />
-                  )}
-                </Link>
-              </div>
-            </div>
-  
-            {/* Responsive Container */}
-  
-            <div
-              className={`${sidebar ? "contact-active" : "contact-active-none"}`}
-            >
-              <div className="responsive-menu-link">
-                <div className="responsive-menu-link-container">
-                  <div className="responsive-menu-link-container-btn">
-                    <Link to="/">Forum</Link>
-                    <Link to="/alldiscussion">All Discussation</Link>
-                    <Link to="/help">Form Guidline</Link>
-                  </div>
+            <div className="responsive-menu-link">
+              <div className="responsive-menu-link-container">
+                <div className="responsive-menu-link-container-btn">
+                  <Link to="/">Forum</Link>
+                  <Link to="/alldiscussion">All Discussation</Link>
+                  <Link to="/help">Form Guidline</Link>
+                </div>
+                {localStorage.getItem("userToken") ? (
                   <div className="topbar-container-btn">
-                    <Link to="/login" className="topbar-container-btn-first">Log In</Link>
+                    <button className="topbar-container-btn-second">
+                      Sign Out
+                    </button>
+                  </div>
+                ) : (
+                  <div className="topbar-container-btn">
+                    <Link to="/login" className="topbar-container-btn-first">
+                      Log Insssssss
+                    </Link>
                     <Link to="/signin" className="topbar-container-btn-second">
                       Sign In
                     </Link>
                   </div>
-                </div>
+                )}
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  };
-  
+      </div>
+    </>
+  );
+};
 
 export default NavbarOne;
