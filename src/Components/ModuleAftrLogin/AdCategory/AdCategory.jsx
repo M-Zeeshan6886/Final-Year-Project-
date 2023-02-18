4422746cd57e2d0505d9c98a8424fec984e90f8b
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2023-02-19 00:04:29 +0500

    Category button add

--- src/Components/ModuleAftrLogin/AdCategory/AdCategory.jsx
+++ src/Components/ModuleAftrLogin/AdCategory/AdCategory.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import './AdCategory.scss';
+import React from "react";
+import "./AdCategory.scss";
 import NavbarOne from "../../../Common Components/Navbar/Navbarbefor";
 import Footer from "../../../Common Components/Footer/Footer";
 const AdCategory = () => {
   return (
     <>
-    <NavbarOne />
+      <NavbarOne />
 
-   <div className="adcategory-container">
-    <div className="adcategory-container-content">
-    <input
-                type="text"
-                placeholder=" Add Category"
-                // value={title}
-                // onChange={(e) => setTitle(e.target.value)}
-              />
-
-    </div>
-     </div>
-    <Footer />
-    
+      <div className="adcategory-container">
+        <div className="adcategory-container-content">
+          <input
+            type="text"
+            placeholder=" Add Category"
+            // value={title}
+            // onChange={(e) => setTitle(e.target.value)}
+          />
+          
+          <button>Create</button>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 };
 
 export default AdCategory;
